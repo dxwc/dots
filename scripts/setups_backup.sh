43c78ffd4de52ee9aa244618fe658d4514abc813
dxwc <dxwc@users.noreply.github.com>
2017-02-09 18:59:01 -0500

    Auto-commit Thu Feb  9 18:59:01 EST 2017

---
+++ scripts/setups_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Commits updates to selected config/settings, scheduled via crontab
+#
+# How-to:
+#- setup github with ssh
+#- https://www.youtube.com/watch?v=UlVqobmcPuM
+#- http://alvinalexander.com/linux/unix-linux-crontab-every-minute-hour-day-syntax
+#- chmod +x this file
+#- sudo vim /etc/crontab and add a entry to this
+#- For hourly I added:
+#- 59 *    * * *   leo     /home/leo/scripts/setups_backup.sh
+
+cd /home/leo/
+git add -u .
+git commit -m "Auto-commit `date`"
+git push -u origin master
+
+exit 0
