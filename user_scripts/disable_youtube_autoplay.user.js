d6c5a724d2a22aa2e2300dc3ed51e7e9822dcfc9
dxwc <dxwc@users.noreply.github.com>
2017-11-24 06:44:30 +0000

    adding include, removing for http

--- user_scripts/disable_youtube_autoplay.user.js
+++ user_scripts/disable_youtube_autoplay.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @description Disable auto-playing of "up next" videos on youtube
-// @match       https://www.youtube.com/watch?v=*
-// @match       http://www.youtube.com/watch?v=*
-// @grant       none
-// @name        Disable youtube autoplay
-// @namespace   dxwc
-// @run-at      document-end
-// @version     0.0.1
+// @description  Disable auto-playing of "up next" videos on youtube
+// @include      https://www.youtube.com/watch?v=*
+// @match        https://www.youtube.com/watch?v=*
+// @grant        none
+// @name         Disable youtube autoplay
+// @namespace    dxwc
+// @run-at       document-end
+// @version      0.0.1
 // ==/UserScript==
 
 if(document.getElementById('autoplay-checkbox').checked)
