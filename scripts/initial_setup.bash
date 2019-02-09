ab0e31db218c43eb164d7b824b13226d3a9b826b
dxwc <dxwc@users.noreply.github.com>
2019-02-09 15:11:08 -0500

    msft font installation instruction

--- scripts/initial_setup.bash
+++ scripts/initial_setup.bash
@@ -19,6 +19,15 @@ sudo eopkg it -c system.devel --yes
 notify-send "NEED PASSWORD" "in initial_setup.bash"
 sudo eopkg install vscode vlc kdenlive gimp simplescreenrecorder guvcview gitg taskwarrior calcurse liferea simple-scan hplip hplip-drivers postgresql sqlite3 mongodb pandoc firefox opera-stable cdrtools iftop zeal gnuplot nasc strace audacity dia macchanger texlive texlive-fonts-extra newsbeuter recoll okular gource --yes
 
+
+notify-send "NEED PASSWORD" "in initial_setup.bash"
+# see: https://getsol.us//articles/software/third-party/en/#microsoft-core-fonts
+# If doesn't work, see also:
+# https://getsol.us/forums/viewtopic.php?t=1733
+# https://github.com/caarlos0-graveyard/msfonts
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/desktop/font/mscorefonts/pspec.xml
+sudo eopkg it mscorefonts*.eopkg;sudo rm mscorefonts*.eopkg
+
 notify-send "NEED PASSWORD" "in initial_setup.bash"
 sudo wget https://raw.githubusercontent.com/dxwc/note/master/note.js -O /usr/local/bin/note
 sudo wget https://raw.githubusercontent.com/dxwc/note/master/gen-pdf.js -O /usr/local/bin/gen-pdf
